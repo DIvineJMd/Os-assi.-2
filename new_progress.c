9a376d4f7b4742ea0d18f5ade2cf9136a8e00eb8
DIvineJMd <101663425+DIvineJMd@users.noreply.github.com>
2022-11-19 10:41:19 +0530

    Update new_progress.c

--- new_progress.c
+++ new_progress.c
@@ -2,33 +2,91 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<time.h>
-#include<sys/time.h>
 #include<pthread.h>
-#include<sched.h>
 #include<unistd.h>
 #include<math.h>
+#define BILLION 1000000000L
+#define NUM_THREAD 3
 void *countA(void *p);
 void *countB(void *p);
 void *countC(void *p);
+pthread_t thr[NUM_THREAD];
+struct timespec s1, e1,s2,e2,s3,e3;
+double accum;
 void Thread_A(){
-    int s;
+    int s,rc;
     struct sched_param prio;
-    pthread_t thr;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    if(pthread_attr_setschedpolicy(&attr,SCHED_OTHER)!=0){fprintf(stderr,"Error in schedulling.");}
-    if(pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED)!=0){fprintf(stderr,"Error in inherating");}
-    if(pthread_attr_setschedparam(&attr,&prio)!=0){fprintf(stderr,"Error in setting priority.");}
-    pthread_create(&thr,&attr,countA,NULL);
-    pthread_join(thr,NULL);
+    int policy=SCHED_OTHER;
+    rc = pthread_setschedparam(thr[0], policy, &prio);
+    printf("A->%d\n",rc);
+    if( clock_gettime( CLOCK_REALTIME, &s1) == -1 ) {
+        fprintf(stderr, "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    pthread_create(&thr[0],NULL,countA,NULL);
+    
     
 }
-
+void Thread_B(){
+     int s,rc;
+    struct sched_param prio;
+    int policy=SCHED_FIFO;
+    rc = pthread_setschedparam(thr[1], policy, &prio);
+    printf("B->%d\n",rc);
+    if( clock_gettime( CLOCK_REALTIME, &s2) == -1 ) {
+        fprintf(stderr, "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    pthread_create(&thr[1],NULL,countB,NULL);
+   
+}
+void Thread_C(){
+      int s,rc;
+    struct sched_param prio;
+    int policy=SCHED_RR;
+    
+    rc = pthread_setschedparam(thr[2], policy, &prio);
+    printf("C->%d\n",rc);
+    if( clock_gettime( CLOCK_REALTIME, &s3) == -1 ) {
+        fprintf(stderr, "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    pthread_create(&thr[2],NULL,countC,NULL);
+  
+}
 //......................................................................
 int main(int argc , char* argv[]){
-    
+    double t1,t2,t3;
     Thread_A();
+    pthread_join(thr[0],NULL);
+    if( clock_gettime( CLOCK_REALTIME, &e1) == -1 ) {
+    fprintf(stderr,"clock gettime" );
+    exit( EXIT_FAILURE );}
+    Thread_B();
     
+    pthread_join(thr[1],NULL);
+    if( clock_gettime( CLOCK_REALTIME, &e2) == -1 ) {
+    fprintf(stderr,"clock gettime" );
+    exit( EXIT_FAILURE );}
+    Thread_C();
+    pthread_join(thr[2],NULL);
+    if( clock_gettime( CLOCK_REALTIME, &e3) == -1 ) {
+    fprintf(stderr,"clock gettime" );
+    exit( EXIT_FAILURE );}
+    
+    
+     t1 = ( e1.tv_sec - s1.tv_sec ) + ( e1.tv_nsec - s1.tv_nsec )/ BILLION;
+     t2 = ( e2.tv_sec - s2.tv_sec ) + ( e2.tv_nsec - s2.tv_nsec )/ BILLION;
+     t3 = ( e3.tv_sec - s3.tv_sec ) + ( e3.tv_nsec - s3.tv_nsec )/ BILLION;
+     printf("\n");
+     int i[3]={t1,t2,t3};
+     for(int j=0;j<3;j++){
+        printf("%d ",i[j]);
+        for(int k=i[j];k>0;k--){
+            printf("*");
+        }
+        printf("\n");
+     }
 }
 //......................................................................
 void *countA(void *p){
@@ -38,6 +96,8 @@ void *countA(void *p){
         c+=1;
     }
     
+    
+     printf("\ndone A\n");
 }
 void *countB(void *p){
     long double c=0;
@@ -46,6 +106,7 @@ void *countB(void *p){
         c+=1;
     }
     
+     printf("\ndone B\n");
 }
 void *countC(void *p){
     long double c=0;
@@ -54,4 +115,8 @@ void *countC(void *p){
         c+=1;
     }
     
+     printf("\ndone c\n");
+    
 }
+
+
